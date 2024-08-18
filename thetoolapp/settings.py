efc5ad34ddca3da7772719da8915e130c8414d0e
ZakO21-ZA <zakariya1345@hotmail.co.uk>
2024-08-18 20:54:54 +0500

    added allowed hosts

--- thetoolapp/settings.py
+++ thetoolapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3w!39zsxjq837d@l^$)ii%t(67vj-jnh5#22^cfl5$m$mc4kdo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['213.210.13.156', '127.0.0.1']
 
 
 # Application definition
